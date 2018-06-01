7fbdb1e6692f23b3f423757f0bb161079a0e2d8f
Narasagounda Patil <36945945+Xam-Techie@users.noreply.github.com>
2018-06-01 18:27:21 +0530

    Commiting new project

---
+++ WebApISQLServer/WebApISQLServer/SampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApISQLServer
+{
+    public class SampleContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Customers.db");
+
+        }
+    }
+}
